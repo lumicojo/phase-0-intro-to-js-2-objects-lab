b48c604309f5c57d0fb157b72db21a00d6182429
+ <+>
2022-10-02 08:53:33 -0400

    commit tree f628a3a900734bae3c9a30e8d8653cd5b62c7820

--- answer.js
+++ answer.js
@@ -1,26 +1,24 @@
-const employee = {
-    name :"Sam",
+const employee  = {
+    name : "Sam",
     streetAddress : "11 Broadway"
 };
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee};
+    const newEmployee = {...employee}
     newEmployee[key] = value;
     return newEmployee;
-}
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
     return employee;
-}
+};
 function deleteFromEmployeeByKey(employee, key) {
     let newEmployee = {...employee};
     delete newEmployee[key];
     return newEmployee;
-}
+};
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
+    delete employee[key];
     return employee;
 }
 
     
-
-
